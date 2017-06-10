906fa9acf284d1e069ae544f57800c82c1950e72
JooJooBee666 <desire4homicide@gmail.com>
2017-06-10 12:44:47 -0400

    griffin: remove unused audio.primary.msm8996 package
    Change-Id: Ifcb594a7e5b4e1e74511e725d295a692b0510ebf

--- device.mk
+++ device.mk
@@ -83,7 +83,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     audiod \
     audio.a2dp.default \
-    audio.primary.msm8996 \
     audio.r_submix.default \
     audio.usb.default \
     libaudio-resampler \
